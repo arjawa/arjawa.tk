9b504890ad55eb2279530e1e3c075c43ce018a43
BRIEXENV <arjawa123@gmail.com>
2021-03-08 20:35:43 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['index.html', 'js/main.js'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'viga': ['Viga'],
+      'custom': ['Custom'],
+      'light': ['Light'],
+      'clean': ['Clean']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
